00db980c5915bcd33e036957aa89d96b3775b59a
pws1453 <pws1453@rit.edu>
2020-03-01 15:05:03 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void hashLoop(){
+    int status;
+    char* command;
+    do{
+        printf("hash>>");
+        scanf("%s",command);
+    }while (status);
+}
+
+int main(int argc,char *argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
